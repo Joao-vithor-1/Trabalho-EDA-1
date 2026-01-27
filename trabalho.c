620ebd3a0ee534895f4ca020bb281eb9674e3f70
Joao-vithor-1 <133594979+Joao-vithor-1@users.noreply.github.com>
2026-01-27 18:49:04 -0300

    commit trabalho.c

--- trabalho.c
+++ trabalho.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+const int temp = 100; //numero temporario ate o professor definir o tamanho da string
+struct carrinho{
+    char produtos[temp][50];
+
+};
+struct client{
+    long long int cpf; //não sei se uso char em vez de long long int, se quiser alterar so trocar na main
+    char nome[temp];
+    long long int telefone;
+    struct carrinho meu_carrinho;
+ };
+struct produto{
+    long long int codico;
+    char nome[temp];
+    float preco;
+
+};
 
