7703bbe05e291a2c1db6ae26d761cee0f7b16748
Warlley Dias <wmedeiros.d@gmail.com>
2026-01-29 08:05:46 -0300

    Update funcoes.h
    adicionado a Remoção de Clientes

--- funcoes.h
+++ funcoes.h
@@ -4,10 +4,15 @@
 #include <stdio.h>
 
 
+//Gerenciamento de Clientes
 
 void Cadastrar_Cliente(client** lista);
 void Listagem_Clientes_Recursiva(client* lista);
 void Buscar_Cliente(client* lista);
 void Editar_Dados(client** lista);
+void Remover_Cliente(client** lista);
+
+//Gerenciamento de produtos
 
 #endif
+
