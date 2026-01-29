b11f716fd2ea8e2a70eb99140bfb4aa5e62a0958
Warlley Dias <wmedeiros.d@gmail.com>
2026-01-29 13:59:12 -0300

    Update funcoes.h
    mudança de editar_dados para editar_dados_cliente

--- funcoes.h
+++ funcoes.h
@@ -9,10 +9,11 @@
 void Cadastrar_Cliente(client** lista);
 void Listagem_Clientes_Recursiva(client* lista);
 void Buscar_Cliente(client* lista);
-void Editar_Dados(client** lista);
+void Editar_Dados_Cliente(client** lista);
 void Remover_Cliente(client** lista);
 
 //Gerenciamento de produtos
 
 #endif
+
 
