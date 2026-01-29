40471bec4d577a9216e620a6a70f90d83ccd75f7
Warlley Dias <wmedeiros.d@gmail.com>
2026-01-29 07:20:35 -0300

    Update funcoes.h
    adicionando a função de editar dados do cliente

--- funcoes.h
+++ funcoes.h
@@ -2,10 +2,12 @@
 #define FUNCOES_H
 #include "structs.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+
+
 
 void Cadastrar_Cliente(client** lista);
 void Listagem_Clientes_Recursiva(client* lista);
 void Buscar_Cliente(client* lista);
+void Editar_Dados(client** lista);
+
 #endif
